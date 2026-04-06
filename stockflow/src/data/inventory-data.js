c88e11cf43aae26c159a5e9f17eae1e00c004830
Tobechi Duru, B.Eng <tobechi.duru001@gmail.com>
2026-04-06 16:18:14 -0700

    Updates!

---
+++ stockflow/src/data/inventory-data.js
@@ -0,0 +1,109 @@
+export const inventory = [{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Parboiled Rice',
+  quantity: 'OK',
+  inStock: '85 kg',
+  minLevel: '20 kg',
+  stockLevel: '85%'
+}, 
+{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Palm Oil',
+  quantity: 'Critical',
+  inStock: '6 L',
+  minLevel: '10 L',
+  stockLevel: '12%'
+}, 
+{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Chicken',
+  quantity: 'Critical',
+  inStock: '4 kg',
+  minLevel: '10 kg',
+  stockLevel: '10%'
+}, 
+{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Beef',
+  quantity: 'Low',
+  inStock: '12 kg',
+  minLevel: '10 kg',
+  stockLevel: '30%'
+}, 
+{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Tomatoes',
+  quantity: 'OK',
+  inStock: '18 kg',
+  minLevel: '5 kg',
+  stockLevel: '60%'
+}, 
+{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Onions',
+  quantity: 'OK',
+  inStock: '22 kg',
+  minLevel: '5 kg',
+  stockLevel: '100%'
+}, 
+{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Ground Crayfish',
+  quantity: 'Low',
+  inStock: '3 kg',
+  minLevel: '2 kg',
+  stockLevel: '30%'
+}, 
+{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Melon (Egusi)',
+  quantity: 'Critical',
+  inStock: '2 kg',
+  minLevel: '3 kg',
+  stockLevel: '13%'
+}, 
+{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Plantain',
+  quantity: 'OK',
+  inStock: '48 fingers',
+  minLevel: '20 fingers',
+  stockLevel: '40%'
+}, 
+{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Soft Drinks',
+  quantity: 'Ok',
+  inStock: '24 bottles',
+  minLevel: '12 bottles',
+  stockLevel: '40%'
+}, 
+{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Cooking Gas',
+  quantity: 'Low',
+  inStock: '8 kg',
+  minLevel: '5 kg',
+  stockLevel: '32%'
+}, 
+{
+  itemId: crypto.randomUUID(),
+  itemImage: '',
+  ingredients: 'Salt',
+  quantity: 'Ok',
+  inStock: '4 kg',
+  minLevel: '1 kg',
+  stockLevel: '40%'
+}, 
+]
